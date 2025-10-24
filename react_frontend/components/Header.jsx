481d93f98dc1f2a4953bf65ebf23fa24e3ff3f67
machumi <petermachumi262@gmail.com>
2025-10-24 09:17:16 +0300

    First commit with warnings like: warning: in the working copy of 'react_frontend/src/main.jsx', LF will be replaced by CRLF the next time Git touches it

---
+++ react_frontend/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <div>
+        <ul>
+            <li>Home</li>
+            <li>Contact</li>
+            <li>Blog</li>
+        </ul>
+    </div>
+    </>
+  )
+}
+
+export default Header
